8d42c85dca0c76983c9d0cdd763c9d348ad7bb90
Alberto Xamin <alberto@xamin.it>
2021-07-10 12:15:02 +0200

    update data source

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const axios = require('axios')
 const schedule = require('node-schedule')
 
+var exec = require('child_process').exec;
+
+var args = (start, end) => `'https://api.performancehorizon.com/v3/partner/analytics/conversions/count' \
+-H 'Connection: keep-alive' \
+-H 'Accept: application/json, text/plain, */*' \
+-H 'Pragma: no-cache' \
+-H 'DNT: 1' \
+-H 'Authorization: ${process.env.AUTH}' \
+-H 'Content-Type: application/json' \
+-H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' \
+-H 'X-Auth-Claim: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb25zb2xlLnBhcnRuZXJpemUuY29tIiwic3ViIjoiMTEwMWw1NDAxMCIsImF1ZCI6ImFwaS5wZXJmb3JtYW5jZWhvcml6b24uY29tIiwiaWF0IjoiMTYyNTkxMDY5MSIsIm5iZiI6IjE2MjU5MTA2OTEiLCJleHAiOiIxNjI1OTk3MDkxIn0.jx1k2BuczdJZ0xwZnibJUQDdiYcUCyJPRYhss9VIR3c' \
+-H 'Sec-GPC: 1' \
+-H 'Origin: https://console.partnerize.com' \
+-H 'Sec-Fetch-Site: cross-site' \
+-H 'Sec-Fetch-Mode: cors' \
+-H 'Sec-Fetch-Dest: empty' \
+-H 'Referer: https://console.partnerize.com/' \
+-H 'Accept-Language: en-US,en;q=0.9' \
+--data-raw '{"scope":{"partner":"1101l64116"},"date_time_ranges":[{"start":"${start}","end":"${end}","field":"date_time"}],"metrics":["total_conversion_items","total_conversions","total_partner_commission","average_order_value","total_order_value"],"timezone":"CET","output_currency":"EUR","filter_by":[{"field":"conversion_type_id","operator":"EQUALS","value":["6"],"not":true}]}' \
+--compressed`;
+
 const fetchData = () => {
 	let date = new Date()
 	let startDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate() - 1, 0, 0, 1))
 	let endDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate() - 1, 23, 59, 59))
-	axios.default.request({
-		url: `https://console.partnerize.com/reporting/report_publisher/publisher/1101l64116/overview/primary/multidate?multidate%5B0%5D%5Bstart_date%5D=${startDate.toISOString()}&multidate%5B0%5D%5Bend_date%5D=${endDate.toISOString()}&multidate%5B0%5D%5Binterval%5D=P1D`,
-		method: 'GET',
-		headers: {
-			'Authorization': process.env.AUTH
-		}
-	}).then(response => {
-		let obj = response.data.period.current[0].totals
-		let message = `PARTNERIZE SUMMARY ${date.toDateString()} \n\n🖱️ ${obj.clicks} Clicks\n✅ ${obj.conversions_all} Conversions\n\nCommission:\nEUR ${obj.currency.EUR.commissions} \nUSD ${obj.currency.USD.commissions}`
+	exec('curl ' + args(startDate.toISOString(), endDate.toISOString()), function (error, stdout, stderr) {
+		console.log('stdout: ' + stdout);
+		let response = JSON.parse(stdout);
+		console.log(response.data)
+		let message = `PARTNERIZE SUMMARY ${date.toDateString()} \n\n ${response.data.total_conversions} Conversions\n\nCommission:\nEUR ${response.data.total_partner_commission}`
+
+		// let obj = response.data.period.current[0].totals
+		// let message = `PARTNERIZE SUMMARY ${date.toDateString()} \n\n🖱️ ${obj.clicks} Clicks\n✅ ${obj.conversions_all} Conversions\n\nCommission:\nEUR ${obj.currency.EUR.commissions} \nUSD ${obj.currency.USD.commissions}`
 		axios.default.post('https://api.telegram.org/bot' + process.env.TG + '/sendMessage',
 			{
 				chat_id: process.env.TG_ID,
@@ -23,7 +43,10 @@ const fetchData = () => {
 			}).catch(error => {
 				console.log(error);
 			});
-	})
+		if (error !== null) {
+			console.log('exec error: ' + error);
+		}
+	});
 }
 
 fetchData()
